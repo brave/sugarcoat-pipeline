711e17099e92bce941275466d6e605d42a29cb77
Shivan Kaul Sahib <shivankaulsahib@gmail.com>
2021-11-16 16:34:01 -0800

    Remove examples

--- examples/simple_example/README.md
+++
@@ -1,1 +0,0 @@
-This is an example test page and script that SugarCoat was run against with a policy.json file.
--- examples/simple_example/script1.js
+++
@@ -1,5 +0,0 @@
-window.onload = () => {
-    console.log(navigator.userAgent);
-    console.log(window.localStorage);
-    console.log(window.performance);
-}
